cff9cc42566aac6e30d554d81d55180c396047ed
AdDoha0 <adamdoxaev@gmail.com>
2025-08-09 17:30:12 +0300

    first commit

---
+++ call-WebRtc/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WebRTCCall from './components/WebRTCCall';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WebRTCCall />
+    </div>
+  );
+}
+
+export default App;
