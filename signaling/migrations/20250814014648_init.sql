6cf54cfd9781afb7d6d131b612725a31b1a2ad95
AdDoha0 <adamdoxaev@gmail.com>
2025-08-14 04:54:37 +0300

    docer -compose

---
+++ signaling/migrations/20250814014648_init.sql
@@ -0,0 +1,27 @@
+-- Только комнаты и участники
+CREATE TABLE rooms (
+    id SERIAL PRIMARY KEY,
+    public_code VARCHAR(8) NOT NULL UNIQUE, 
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    is_active BOOLEAN DEFAULT true
+);
+
+CREATE TABLE participants (
+    id SERIAL PRIMARY KEY,
+    room_id INTEGER REFERENCES rooms(id) ON DELETE CASCADE,
+    client_id NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    is_host BOOLEAN DEFAULT false,
+    UNIQUE(room_id, client_id)
+);
+
+
+-- Таблица сообщений чата
+CREATE TABLE chat_messages (
+    id SERIAL PRIMARY KEY,
+    room_id INTEGER NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
+    sender_id INTEGER NOT NULL REFERENCES participants(id) ON DELETE CASCADE,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+);
